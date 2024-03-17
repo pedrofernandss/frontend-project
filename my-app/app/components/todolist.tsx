d58f82addb68decdcb99a34c248f1e7f755585df
Pedro Fernandes <pedro.fernandes@snowfox-ai.com>
2024-03-16 22:39:27 -0300

    day 01

---
+++ my-app/app/components/todolist.tsx
@@ -0,0 +1,27 @@
+import { ITask } from "@/types/tasks";
+import { Task } from "./task";
+
+interface TodoListProps {
+  tasks: ITask[]
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
+    return <div className="overflow-x-auto">
+    <table className="table table-zebra">
+      {/* head */}
+      <thead>
+        <tr>
+          <th>Tasks</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tasks.map((task) => (
+          <Task key={task.id} task={task}/>
+        ))}
+      </tbody>
+    </table>
+  </div>
+};
+
+export default TodoList
